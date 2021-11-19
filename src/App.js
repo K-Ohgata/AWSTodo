166b47de9ebd565c88f2abc7cf7cc5d8f6ba8912
K-Ohgata <oh.kazuma1017@gmail.com>
2021-11-19 10:24:33 +0900

    add auth again

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
+import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import { withAuthenticator, AmplifySignOut } from '@aws-amplify/ui-react'
 
 function App() {
+  
   return (
     <div className="App">
       <header className="App-header">
